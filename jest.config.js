bc241964ccad5b253959457d599ee5db3754276d
Hao Luo <howlowck@gmail.com>
2022-01-04 10:34:51 -0600

    added jest vscode extension

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // source: https://github.com/tomchen/example-typescript-package
 
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
